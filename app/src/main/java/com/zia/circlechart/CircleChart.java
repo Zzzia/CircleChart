244557a73817a04fa9205de727faed877e1d2a68
zia <jzljzljl@outlook.com>
2017-08-01 21:34:19 +0800

    优化了性能，封装完毕

--- app/src/main/java/com/zia/circlechart/CircleChart.java
+++ app/src/main/java/com/zia/circlechart/CircleChart.java
@@ -5,11 +5,11 @@ import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.RectF;
-import android.os.Build;
 import android.support.annotation.Nullable;
 import android.util.AttributeSet;
 import android.util.Log;
-import android.widget.TextView;
+
+import java.util.List;
 
 /**
  * Created by zia on 17-8-1.
@@ -17,15 +17,16 @@ import android.widget.TextView;
 
 public class CircleChart extends android.support.v7.widget.AppCompatTextView {
 
-    private boolean isLog = true;
+    private boolean isLog = false;
     private Paint paint;//画笔
-    private float radius = 100;//半径
-    private float pointWidth = 100;//原点宽度
-    private float spaceWidth = 20;//间隔宽度
+    private int paintWidth = 30;//线宽
     private int speed = 1;//移动速度
-    private float centerPointX;//圆心x位置
-    private float centerPointY;
-    private int rotate = 0;//转动
+    private int rotate = 0;//转动角度
+    private boolean isRun = false;
+    private List<ChartData> list;//数据
+    private int space = 100;//设置为自动计算间距，此项废弃
+    private float centerX,centerY;
+    static int defaultColor = Color.RED,defaultTextColor = Color.BLACK;
 
 
     public CircleChart(Context context) {
@@ -39,45 +40,161 @@ public class CircleChart extends android.support.v7.widget.AppCompatTextView {
     public CircleChart(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
         paint = new Paint();
-        paint.setStrokeCap(Paint.Cap.ROUND);
-        paint.setStyle(Paint.Style.STROKE);
-        paint.setStrokeWidth(20);//描边宽度
-        paint.setAntiAlias(true);//抗锯齿
-        paint.setDither(true);//防抖动
 
     }
 
     @Override
     protected void onDraw(final Canvas canvas) {
         super.onDraw(canvas);
-        new Thread(new Runnable() {
-            @Override
-            public void run() {
-                drawArc(canvas,50,Color.RED,100);
-                drawArc(canvas,80,Color.GREEN,200);
+        if(isRun) {
+            int count = 0;
+            int space = (getWidth()/2-10)/list.size();
+            for(ChartData data : list){
+                if(data.getRadius() == 0){
+                    data.setRadius(getWidth()/2-20-space*count);
+                    count++;
+                }
+                //drawArc(canvas, data.getPercentage(), data.getColor()+200, data.getRadius()+3,data.getSpeed());
+                drawArc(canvas, data.getPercentage(), data.getColor(), data.getRadius(),data.getSpeed());
+                drawText(canvas,data.getText(),paintWidth,data.getTextColor(),data.getRadius());
             }
-        }).run();
+        }
     }
 
-    void drawArc(Canvas canvas, float percentage, int color, float radius){
-        float centerX = getWidth()/2;
-        float centerY = getHeight()/2;
+    /**
+     * 画圆方法封装
+     * @param canvas 画布
+     * @param percentage 百分比
+     * @param color 圆圈颜色
+     * @param radius 半径
+     * @param s 速度
+     */
+    private void drawArc(Canvas canvas, float percentage, int color, float radius, int s){
+        centerX = getWidth()/2;
+        centerY = getHeight()/2;
         RectF oval = new RectF( centerX-radius, centerY-radius, centerX+radius, centerY+radius);//用一个正方形包裹圆形
+        paint.setStrokeCap(Paint.Cap.ROUND);
+        paint.setStyle(Paint.Style.STROKE);
+        paint.setStrokeWidth(paintWidth);//描边宽度
+        paint.setAntiAlias(true);//抗锯齿
+        paint.setDither(true);//防抖动
         paint.setColor(color);
         if((float)rotate/360*100 < percentage){//还没到达终点
-            rotate++;
+            rotate = rotate + s;
             canvas.drawArc(oval,-90,rotate,false,paint);
             log(rotate+"");
+            invalidate();
         }else{//到达终点
             canvas.drawArc(oval,-90,percentage/100*360,false,paint);
-
         }
-        invalidate();
     }
 
+    private void drawText(Canvas canvas,String text,int textSize,int color,float radius){
+        paint.setColor(color);
+        paint.setStyle(Paint.Style.FILL);
+        paint.setTextSize(textSize);
+        canvas.drawText(text,centerX-paint.measureText(text)-15,centerY-radius+10,paint);
+    }
 
+    public void setData(List<ChartData> list){
+        this.list = list;
+    }
 
-    void log(String msg){
+    public void setDefaultColor(int color){
+        defaultColor = color;
+    }
+
+    public void setDefaultTextColor(int color){
+        defaultTextColor = color;
+    }
+
+    /**
+     * 设置速度，默认为1，每秒走1/6圆
+     * 若设置为2，每秒走1/3圈
+     * 公式 6/速度
+     * @param speed 速度
+     */
+    public void setSpeed(int speed){
+        this.speed = speed;
+    }
+
+    /**
+     * 设置圆圈半径粗细
+     * @param width 宽度
+     */
+    public void setPaintWidth(int width){
+        paintWidth = width;
+    }
+
+    /**
+     * 开始动画
+     */
+    public void run(){
+        isRun = true;
+    }
+
+    public void openLog(){
+        isLog = true;
+    }
+
+    private void log(String msg){
         if(isLog) Log.d("CircleChartTest",msg);
+    }
+}
+
+class ChartData{
+    private int percentage = 0;
+    private int color = CircleChart.defaultColor;
+    private int textColor = CircleChart.defaultTextColor;
+    private int speed = 1;
+    private String text = "data";
+    private float radius = 0;
+
+    public int getTextColor() {
+        return textColor;
+    }
+
+    public void setTextColor(int textColor) {
+        this.textColor = textColor;
+    }
+
+    public float getRadius() {
+        return radius;
+    }
+
+    public void setRadius(float radius) {
+        this.radius = radius;
+    }
+
+    public int getPercentage() {
+        return percentage;
+    }
+
+    public void setPercentage(int percentage) {
+        this.percentage = percentage;
+    }
+
+    public int getColor() {
+        return color;
+    }
+
+    public void setColor(int color) {
+        this.color = color;
+    }
+
+    public int getSpeed() {
+        return speed;
+    }
+
+    public void setSpeed(int speed) {
+        this.speed = speed;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
     }
 }
--- app/src/main/java/com/zia/circlechart/MainActivity.java
+++ app/src/main/java/com/zia/circlechart/MainActivity.java
@@ -1,13 +1,54 @@
 package com.zia.circlechart;
 
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
+
+    List<ChartData> datas = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initData();
+        CircleChart circleChart = (CircleChart)findViewById(R.id.chart);
+        //circleChart.setPaintWidth(30);//设置线宽默认为30
+        //速度默认为1.具体见代码注释
+        // 这里设置为所有圆的速度，可在ChartData里单独设置
+        //circleChart.setSpeed(2);
+        //circleChart.setDefaultColor(Color.parseColor("#bfbfbf"));//设置默认线颜色
+        //circleChart.setDefaultTextColor(Color.parseColor("#bfbfbf"));//设置字体颜色
+        circleChart.setData(datas);
+        //开始动画，在这之前不会动
+        circleChart.run();
+    }
+
+    private void initData(){
+        ChartData data1 = new ChartData();
+        ChartData data2 = new ChartData();
+        ChartData data3 = new ChartData();
+        ChartData data4 = new ChartData();
+        data1.setPercentage(90);
+        data1.setColor(Color.RED);
+        data1.setText("妹子: 90%");
+        data1.setTextColor(Color.parseColor("#FFCCCC"));
+        data2.setPercentage(70);
+        data2.setColor(Color.GREEN);
+        data2.setText("学渣: 70%");
+        data3.setPercentage(50);
+        data3.setColor(Color.BLACK);
+        data3.setText("好盆友: 50%");
+        data4.setPercentage(30);
+        data4.setColor(Color.GRAY);
+        data4.setText("土豪: 30%");
+        datas.add(data1);
+        datas.add(data2);
+        datas.add(data3);
+        datas.add(data4);
     }
 }
